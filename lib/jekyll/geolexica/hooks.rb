0ca91917058add37db5c716ffc4b1b0319809301
Sebastian Skałacki <skalee@gmail.com>
2020-04-23 19:16:31 +0200

    Move data loading from generator to Glossary

--- lib/jekyll/geolexica/hooks.rb
+++ lib/jekyll/geolexica/hooks.rb
@@ -5,12 +5,18 @@ module Jekyll
 
       def register_all_hooks
         Jekyll::Hooks.register :site, :after_init, &method(:initialize_glossary)
+        Jekyll::Hooks.register :site, :post_read, &method(:load_glossary)
       end
 
       # Adds Jekyll::Site#glossary method, and initializes an empty glossary.
       def initialize_glossary(site)
         site.class.attr_reader :glossary
         site.instance_variable_set "@glossary", Glossary.new(site)
+      end
+
+      # Loads concept data into glossary.
+      def load_glossary(site)
+        site.glossary.load_glossary
       end
     end
   end
