cd183b970d465642d610180d9ca9649afb03b694
Sebastian Skałacki <skalee@gmail.com>
2020-05-19 17:49:15 +0200

    Use ConceptSerializer for JSON output

--- lib/jekyll/geolexica/concept_serializer.rb
+++ lib/jekyll/geolexica/concept_serializer.rb
@@ -14,6 +14,27 @@ module Jekyll
         super(concept)
         @site = site
       end
+
+      def to_json
+        JSON.dump(in_all_languages)
+      end
+
+      private
+
+      # Returns concept hash in all supported languages, with +nil+ value for
+      # every supported language that this concept is not translated to.
+      def in_all_languages(concept_hash = data)
+        hash_keys = NON_LANGUAGE_KEYS + term_languages
+        slice_hash_with_default(concept_hash, nil, *hash_keys)
+      end
+
+      # Like Hash#slice, but takes a +default_value+, which is used for every
+      # key not present in the +hash+.
+      def slice_hash_with_default(hash, default_value, *keys)
+        h = hash.dup
+        keys.each { |k| h[k] ||= default_value }
+        h.slice(*keys)
+      end
     end
   end
 end
