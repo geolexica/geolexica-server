d0ac16756831657351f2671b22e181a15a9b8a31
Sebastian Skałacki <skalee@gmail.com>
2020-05-22 18:14:47 +0200

    Add lib/jekyll-geolexica.rb to bridge conventions
    The Jekyll convention is to use kebab-case in gem names only, even
    though it conflicts with Ruby conventions.  A special file is needed
    to bridge those conventions together, otherwise this plugin won't
    work if loaded via _config.yml.

---
+++ lib/jekyll-geolexica.rb
@@ -0,0 +1,5 @@
+# The Jekyll convention is to use kebab-case in gem names only, even though
+# it conflicts with Ruby conventions.  This file is meant to bridge those
+# two conventions together, otherwise this plugin won't work if loaded
+# via _config.yml.
+require "jekyll/geolexica"
