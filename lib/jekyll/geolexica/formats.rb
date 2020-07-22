e06dba8ae7d0b9d6b3f6d6580e78d5b11f5bfd59
Sebastian Skałacki <skalee@gmail.com>
2020-07-22 16:34:38 +0200

    wip maybe

--- lib/jekyll/geolexica/formats.rb
+++ lib/jekyll/geolexica/formats.rb
@@ -3,15 +3,34 @@
 
 module Jekyll
   module Geolexica
-    FormatDefinition = Struct.new(:setting_name, keyword_init: true)
+    FormatDefinition = Struct.new(:setting_name, :page_class_name,
+      keyword_init: true)
 
     FORMATS = {
-      html: {setting_name: "html"},
-      json: {setting_name: "json"},
-      jsonld: {setting_name: "json-ld"},
-      tbx: {setting_name: "tbx-iso-tml"},
-      turtle: {setting_name: "turtle"},
-      yaml: {setting_name: "yaml"},
+      html: {
+        setting_name: "html",
+        page_class_name: "ConceptPage::HTML",
+      },
+      json: {
+        setting_name: "json",
+        page_class_name: "ConceptPage::JSON",
+      },
+      jsonld: {
+        setting_name: "json-ld",
+        page_class_name: "ConceptPage::JSONLD",
+      },
+      tbx: {
+        setting_name: "tbx-iso-tml",
+        page_class_name: "ConceptPage::TBX",
+      },
+      turtle: {
+        setting_name: "turtle",
+        page_class_name: "ConceptPage::Turtle",
+      },
+      yaml: {
+        setting_name: "yaml",
+        page_class_name: "ConceptPage::YAML",
+      },
     }.
       transform_values { |v| FormatDefinition.new(**v).freeze }.
       freeze
