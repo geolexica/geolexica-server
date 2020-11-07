0281e1eac0a00888bb0ad53ef18b06b668c65a9f
Sebastian Skałacki <skalee@gmail.com>
2020-11-07 06:54:01 +0100

    Write RDoc for Jekyll::Geolexica.load_tasks

--- lib/jekyll/geolexica.rb
+++ lib/jekyll/geolexica.rb
@@ -5,6 +5,13 @@ require "jekyll"
 
 module Jekyll
   module Geolexica
+    # Loads Rake tasks which are provided in this gem.
+    #
+    # In order to load these tasks, following lines should be added to given
+    # site's Rakefile:
+    #
+    #   require "jekyll-geolexica"
+    #   ::Jekyll::Geolexica.load_tasks
     def self.load_tasks
       tasks_path = File.expand_path("../tasks", __dir__)
       Rake.add_rakelib(tasks_path)
