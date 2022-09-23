671c72ccd436a707461ec6df37d39a0551da733d
HassanAkbar <hassanakbar2@gmail.com>
2022-09-23 15:14:57 +0500

    added specs and fixed linting issues

--- lib/geolexica_server/generator/base.rb
+++ lib/geolexica_server/generator/base.rb
@@ -11,12 +11,14 @@ module GeolexicaServer
       end
 
       def initialize(options = {})
-        config_file_path = options["config_file_path"] || GeolexicaServer::DEFAULT_CONFIG_FILE_PATH
+        config_file_path = options["config_file_path"] ||
+                           GeolexicaServer::DEFAULT_CONFIG_FILE_PATH
 
         @config = YAML.load_file(config_file_path)
 
-        if @config["geolexica"].nil? || @config["geolexica"]["glossary_path"].nil?
-          raise StandardError, "geolexica.glossary_path is missing in config file."
+        unless @config["geolexica"] && @config["geolexica"]["glossary_path"]
+          raise StandardError,
+                "geolexica.glossary_path is missing in config file."
         end
 
         @glossary_path = @config["geolexica"]["glossary_path"]
--- spec/unit/geolexica_server/generator/metadata_spec.rb
+++ spec/unit/geolexica_server/generator/metadata_spec.rb
@@ -3,4 +3,51 @@
 require "spec_helper"
 
 RSpec.describe GeolexicaServer::Generator::Metadata do
+  let(:options) { { "config_file_path" => fixture_path("config.yaml") } }
+  subject { described_class.new(options) }
+
+  describe "#extract_terms" do
+    let(:terms) { subject.send(:extract_terms) }
+    let(:expected_output) do
+      [
+        {
+          "termid" => "3.1.1.2",
+          "term" => "immaterial entity",
+          "eng" => {
+            "terms" => [
+              { "type" => "expression", "designation" => "immaterial entity" },
+            ],
+          },
+        },
+        {
+          "termid" => "3.1.1.1",
+          "term" => "entity",
+          "eng" => {
+            "terms" => [
+              { "type" => "expression", "designation" => "entity" },
+            ],
+          }
+        },
+      ]
+    end
+
+    it "should return terms from concepts" do
+      expect(terms).to eq(expected_output)
+    end
+  end
+
+  describe "#metadata" do
+    let(:terms) { subject.send(:extract_terms) }
+    let(:expected_output) do
+      {
+        "concept_count" => 2,
+        "term_count" => 2,
+        "version" => "20220530",
+      }
+    end
+
+    it "should return terms metadata" do
+      expect(subject.send(:metadata, terms)).to eq(expected_output)
+    end
+  end
 end
